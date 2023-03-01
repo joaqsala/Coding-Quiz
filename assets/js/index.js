4ad02646d1135a37335dbe646ff60369f38fcda1
Joaquin Salazar <joaqsala@gmail.com>
2023-03-01 15:54:15 -0600

    added starting html and css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,13 @@
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
+// create html file with start button, highscore, and timer graphics
 
+//add event listener to timer to start timer and quiz
 
-## Acceptance Criteria
+// present the first question and start a timer
 
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and my score
+//create an iteration to choose questions about JS
+
+//incorrect responses subtract time from the timer
+
+//when all questions are answered or time reaches 0 the game is over
+
+//when the game is over, save initials and my score
