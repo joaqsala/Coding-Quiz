7e585ccb36405cfed45ae172b50db611dd1a83a7
Joaquin Salazar <joaqsala@gmail.com>
2023-03-02 12:55:21 -0600

    addEventListener attached to timer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,33 @@
-// create html file with start button, highscore, and timer graphics
+var timerDisplay = document.querySelector(".show-time");
 
 //add event listener to timer to start timer and quiz
+document.querySelector(".begin-quiz").addEventListener('click', countdown);
+
+function countdown(){
+    //timer that counts down from 60 seconds
+    var timeLeft = 60;
+    //`setInterval()` to call a function that executes every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            // show the remaining seconds and decrease timer
+            timerDisplay.textContent = timeLeft + ' seconds';
+            timeLeft--;
+          } else if (timeLeft === 1) {
+            timerDisplay.textContent = timeLeft + ' seconds';
+            timeLeft--;
+          } else {
+            // display Game Over
+            timerDisplay.textContent = 'GAME OVER';
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+            // Call the `displayMessage()` function
+            displayMessage();
+          }
+        }, 1000);
+      }
+
+    //   WRITE DISPLAY MessageChannel function
+
 
 // present the first question and start a timer
 
